7346cb9a3386d7c018fea05e0358a18bf83db5ce
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-04-09 21:34:17 +0300

    create OurTest Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Component/Frame1/Header";
 import Dreams from "./Component/Frame2/Dreams";
+import OurTest from "./Component/Frame3/OurTest"
 
 function App() {
   return (
    <>
    <Header/>
    <Dreams/>
+   <OurTest/>
    </>
   );
 }
--- src/Component/Frame3/OurTest.jsx
+++ src/Component/Frame3/OurTest.jsx
@@ -3,35 +3,35 @@ const OurTest = () => {
         <>
         <div className="blockProgram">
             <div className="infoBlockProgram">
-                <p>OUR TESTS</p>
-                    <h2>Start your program with our at-home test.</h2>
-                        <p>
+                <p className="infoBlockProgramP">OUR TESTS</p>
+                    <h2 className="infoBlockProgramH2">Start your program with our at-home test.</h2>
+                        <p className="infoBlockProgramP2">
                             One-size-fits-all advice doesn't work. So we'll 
                             provide you with the most advanced analysis of your gut bacteria,
                             blood sugar and blood fats available. We'll then compare your results 
                             to thousands of other people we've tested. Using patented techniques 
                             you can't get anywhere else.
                         </p>
-                <button>Discover our tests</button>
+                <button className="infoBlockProgramButton">Discover our tests</button>
             </div>
             <div className="blockProgramImg">
-                <img src="" alt="" />
+                <img src="./img/apple.png" alt="" />
             </div>
         </div>
         <div className="blockResult">
             <div className="blockResultImg">
-                <img src="" alt="" />
+                <img src="./img/snack.png" alt="" />
             </div>
             <div className="infoBlockResult">
-                <p>YOUR PROGRAM</p>
-                <h2>Get personalized advice powered by your unique test results</h2>
-                    <p>
+                <p className="infoBlockResultP">YOUR PROGRAM</p>
+                <h2 className="infoBlockResultH2">Get personalized advice powered by your unique test results</h2>
+                    <p className="infoBlockResultP2">
                         On the ZOE Program, no food is off limits because science shows 
                         restricting yourself just doesn’t work. So, we'll teach you how 
                         to make smart combos each week, using insights from your tests 
                         to improve your meals.
                     </p>
-                <button>See how it works</button>
+                <button className="infoBlockResultButton">See how it works</button>
             </div>
         </div>
         </>
